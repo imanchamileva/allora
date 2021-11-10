5642716d46e3dd12c9bb3af221093fd585afb39d
Iman Chamileva <iman.moussaevna@gmail.com>
2021-11-10 11:40:38 +0100

    menu ok

--- views/html/menu.php
+++ views/html/menu.php
@@ -1,33 +1,37 @@
-<nav>
-  <ul>
-    <li><a href="?section=accueil">Accueil</a> </li>
-    <li><a href="?section=femme">Femme</a>
+<div class="themenu">
+  <nav>
+    <div>
       <ul>
-        <li> <a href="robes.html">Robes</a> </li>
-        <li> <a href="chemisiers.html">Chemisiers</a> </li>
-        <li> <a href="vestes.html">Vestes</a> </li>
-        <li> <a href="pantalons.html">Pantalons</a> </li>
-        <li> <a href="jupes.html">Jupes</a> </li>
+        <li><a href="?section=accueil">Accueil</a> </li>
+        <li><a href="?section=femme">Femme</a>
+          <ul>
+            <li> <a href="robes.html">Robes</a> </li>
+            <li> <a href="chemisiers.html">Chemisiers</a> </li>
+            <li> <a href="vestes.html">Vestes</a> </li>
+            <li> <a href="pantalons.html">Pantalons</a> </li>
+            <li> <a href="jupes.html">Jupes</a> </li>
+          </ul>
+        </li>
+        <li><a href="">Homme</a>
+          <ul>
+            <li> <a href="chemises.html">Chemises</a> </li>
+            <li> <a href="tshirts.html">T-shirts</a> </li>
+            <li> <a href="sweatsacapuche.html">Sweats à capuche</a> </li>
+            <li> <a href="pantalons.html">Pantalons</a> </li>
+            <li> <a href="shorts.html">Shorts</a> </li>
+          </ul>
+        </li>
+        <li> <a href="enfant.html">Enfant</a> </li>
+        <span id="loggo">
+          <img src="public/images/loggo.png" alt="">
+        </span>
+    
+      <span class="menudroite" id="life">
+        <li><a href="?section=connexion">Connexion</a></li>
+        <li><a href="favoris.html">Favoris</a></li>
+        <li><a href="panier.html">Panier</a></li>
+      </span>
       </ul>
-    </li>
-    <li><a href="">Homme</a>
-      <ul>
-        <li> <a href="chemises.html">Chemises</a> </li>
-        <li> <a href="tshirts.html">T-shirts</a> </li>
-        <li> <a href="sweatsacapuche.html">Sweats à capuche</a> </li>
-        <li> <a href="pantalons.html">Pantalons</a> </li>
-        <li> <a href="shorts.html">Shorts</a> </li>
-      </ul>
-    </li>
-    <li> <a href="enfant.html">Enfant</a> </li>
-    <span id="loggo">
-      <img src="public/images/loggo.png" alt="">
-    </span>
+  </nav>
 
-    <span class="menudroite" id="life">
-      <li><a href="?section=connexion">Connexion</a></li>
-      <li><a href="favoris.html">Favoris</a></li>
-      <li><a href="panier.html">Panier</a></li>
-    </span>
-  </ul>
-</nav>
+</div>
